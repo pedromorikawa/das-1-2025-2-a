5a2290f1680c711a331735131b3dd7a65ca2b248
pedromorikawa <127156227+pedromorikawa@users.noreply.github.com>
2025-09-30 23:04:25 +0000

    aula-30-09

---
+++ eventos/src/main/java/br/univille/Producer.java
@@ -0,0 +1,28 @@
+package br.univille;
+
+import com.azure.core.amqp.AmqpTransportType;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+
+public class Producer {
+    public static void main(String[] args) {
+        var servidor = "sb-das1-2025.servicebus.windows.net";
+
+        String chave = System.getenv("CHAVE");
+        String queue = "queue-pedromorikawa";
+
+        ServiceBusSenderClient senderClient =
+            new ServiceBusClientBuilder()
+            .fullyQualifiedNamespace(servidor)
+            .connectionString(chave)
+            .transportType(AmqpTransportType.AMQP_WEB_SOCKETS)
+            .sender()
+            .queueName(queue)
+            .buildClient();
+            
+        senderClient.sendMessage(new ServiceBusMessage("1: Teste"));
+        senderClient.sendMessage(new ServiceBusMessage("2: Eu não acredito"));
+        senderClient.sendMessage(new ServiceBusMessage("3: Bom dia!"));
+    }
+}
